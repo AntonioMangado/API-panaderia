ac91b43e043d2d5f7e97362b3062c8964c29c079
Mangui <antonio.mangado@gmail.com>
2024-06-13 10:36:35 +0200

    ADD/ formatting to queries and config

--- queries/insert_into.sql
+++ queries/insert_into.sql
@@ -1,19 +1,19 @@
 INSERT INTO products (title, description, status) values ('Eclair', 'French pastry filled with cream', 'stock');
 
-INSERT INTO products (title, description, status) VALUES ('Croissant', 'Buttery, flaky, and crescent-shaped pastry', 'no stock');
+INSERT INTO products (title, description, status) values ('Croissant', 'Buttery, flaky, and crescent-shaped pastry', 'no stock');
 
-INSERT INTO products (title, description, status) VALUES ('Baguette', 'Long, thin loaf of French bread', 'stock');
+INSERT INTO products (title, description, status) values ('Baguette', 'Long, thin loaf of French bread', 'stock');
 
-INSERT INTO products (title, description, status) VALUES ('Macaron', 'Sweet meringue-based confection', 'no stock');
+INSERT INTO products (title, description, status) values ('Macaron', 'Sweet meringue-based confection', 'no stock');
 
-INSERT INTO products (title, description, status) VALUES ('Tart', 'Pastry crust filled with fruit or custard', 'stock');
+INSERT INTO products (title, description, status) values ('Tart', 'Pastry crust filled with fruit or custard', 'stock');
 
-INSERT INTO products (title, description, status) VALUES ('Quiche', 'Savoury pie filled with egg and other ingredients', 'no stock');
+INSERT INTO products (title, description, status) values ('Quiche', 'Savoury pie filled with egg and other ingredients', 'no stock');
 
-INSERT INTO products (title, description, status) VALUES ('Brioche', 'Soft, sweet, and buttery bread', 'stock');
+INSERT INTO products (title, description, status) values ('Brioche', 'Soft, sweet, and buttery bread', 'stock');
 
-INSERT INTO products (title, description, status) VALUES ('Pain au Chocolat', 'Flaky pastry with chocolate filling', 'no stock');
+INSERT INTO products (title, description, status) values ('Pain au Chocolat', 'Flaky pastry with chocolate filling', 'no stock');
 
-INSERT INTO products (title, description, status) VALUES ('Madeleine', 'Small, shell-shaped sponge cakes', 'stock');
+INSERT INTO products (title, description, status) values ('Madeleine', 'Small, shell-shaped sponge cakes', 'stock');
 
-INSERT INTO products (title, description, status) VALUES ('Crème Brûlée', 'Rich custard dessert with caramelized sugar top', 'no stock');
+INSERT INTO products (title, description, status) values ('Crème Brûlée', 'Rich custard dessert with caramelized sugar top', 'no stock');
